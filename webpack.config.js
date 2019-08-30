409fc8211b67a735d5c4f1a7ef2adcd60f2cdd33
Kamonohashi <kamonotakahashi@gmail.com>
2019-08-30 18:33:41 +0900

    development追加　inline-source-map

--- webpack.config.js
+++ webpack.config.js
@@ -3,69 +3,72 @@ var webpack = require('webpack');
 const { VueLoaderPlugin } = require("vue-loader");
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: `./src/index.js`,
-    mode: "development",
-    output: {
-        path: path.resolve(__dirname, './dist/'),
-        publicPath: "/",
-        filename: "main.js",
-    },
-    module: {
-        rules: [
-           {
-             test: /\.vue$/,
-             loader: "vue-loader",
-           },
-           {
-             test: /\.vue\.html$/,
-             loader: "vue-loader"
-         },
-         {
-           test: /\.js$/,
-           exclude: /node_modules/,
-           use: {
-               loader: "babel-loader",
-           }
-         },
-           {
-             test: /\.css$/,
-             loader: 'style-loader!css-loader',
-             //loader: "style-loader"
-         },
-         {
-             test: /\.(jpg|png|svg|gif)$/,
-             loaders: 'file-loader?name=[name].[ext]'
-         },
-            {
-              test: /\.scss$/,
-              use: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: ['css-loader', 'sass-loader']
-              })
-          },
-            {
-              test: /\.(otf|eot|svg|ttf|woff|woff2)(\?.+)?$/,
-              loader: 'url-loader'
-            },
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin('style.css'),
-        new VueLoaderPlugin()
-    ],
-    devtool: "inline-source-map",
-    devServer: {
-        contentBase: path.join(__dirname, './dist/'),
-        host: "127.0.0.1",
-        port: 8080,
-        open: true,
-        historyApiFallback: true,
-    },
-    resolve: {
-        extensions: ['.js', '.vue'],
-        alias: {
-            'vue$': 'vue/dist/vue.js'
+module.exports = (env, argv) => {
+    const IS_DEVELOPMENT = argv.mode === 'development';
+    return {
+        entry: `./src/index.js`,
+        mode: "development",
+        output: {
+            path: path.resolve(__dirname, './dist/'),
+            publicPath: "/",
+            filename: "main.js",
+        },
+        devtool: IS_DEVELOPMENT ? 'inline-source-map' : 'none',
+        module: {
+            rules: [
+               {
+                 test: /\.vue$/,
+                 loader: "vue-loader",
+               },
+               {
+                 test: /\.vue\.html$/,
+                 loader: "vue-loader"
+             },
+             {
+               test: /\.js$/,
+               exclude: /node_modules/,
+               use: {
+                   loader: "babel-loader",
+               }
+             },
+               {
+                 test: /\.css$/,
+                 loader: 'style-loader!css-loader',
+                 //loader: "style-loader"
+             },
+             {
+                 test: /\.(jpg|png|svg|gif)$/,
+                 loaders: 'file-loader?name=[name].[ext]'
+             },
+                {
+                  test: /\.scss$/,
+                  use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                  })
+              },
+                {
+                  test: /\.(otf|eot|svg|ttf|woff|woff2)(\?.+)?$/,
+                  loader: 'url-loader'
+                },
+            ]
+        },
+        plugins: [
+            new ExtractTextPlugin('style.css'),
+            new VueLoaderPlugin()
+        ],
+        devServer: {
+            contentBase: path.join(__dirname, './dist/'),
+            host: "127.0.0.1",
+            port: 8080,
+            open: true,
+            historyApiFallback: true,
+        },
+        resolve: {
+            extensions: ['.js', '.vue'],
+            alias: {
+                'vue$': 'vue/dist/vue.js'
+            }
         }
-    },
+    }
 };

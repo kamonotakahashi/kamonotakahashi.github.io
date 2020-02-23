2cca9c660079fc5f08622d8e4b259e7ff557e989
kamonotakahashi <kamonotakahashi@gmail.com>
2020-02-23 17:14:06 +0900

    commit

--- src/components/globals/TheHeader.vue
+++ src/components/globals/TheHeader.vue
@@ -1,24 +1,24 @@
 <template>
     <nav class="nav">
-        <div class="logo">
+        <div class="nav__logo">
             <div class="logo__title">SZK LABO</div>
         </div>
-        <ul class="nav-links">
-            <li><router-link to="/" class="spa-link">Home</router-link></li><span></span>
-            <li><router-link to="/skills" class="spa-link">About</router-link></li><span></span>
-            <li><router-link to="/jobs" class="spa-link">Works</router-link></li><span></span>
-            <li><router-link to="/" class="spa-link">Contact</router-link></li>
+        <ul class="nav__link">
+            <li><router-link to="/">Home</router-link></li><span></span>
+            <li><router-link to="/skills">About</router-link></li><span></span>
+            <li><router-link to="/jobs">Works</router-link></li><span></span>
+            <li><router-link to="/">Contact</router-link></li>
         </ul>
-        <div class="burger" id="burger_item" @click="burgerMenuToggle" :class="{active:burgerMenu_state}">
+        <div class="nav__burger" id="burger_item" @click="burgerMenuToggle" :class="{active:burgerMenu_state}">
             <span class="burger-line"></span>
             <span class="burger-line"></span>
             <span class="burger-line"></span>
         </div>
-        <ul :class="['nav-slider' , {'active':burgerMenu_state}]">
-            <li class="slider-spa-link" @click="mobileMenu"><router-link to="/" class="link">Home</router-link></li>
-            <li class="slider-spa-link" @click="mobileMenu"><router-link to="/skills" class="link">About</router-link></li>
-            <li class="slider-spa-link" @click="mobileMenu"><router-link to="/jobs" class="link">Works</router-link></li>
-            <li class="slider-spa-link" @click="mobileMenu"><router-link to="/" class="link">Contact</router-link></li>
+        <ul :class="['nav__slider' , {'active':burgerMenu_state}]">
+            <li @click="mobileMenu"><router-link to="/" class="link">Home</router-link></li>
+            <li @click="mobileMenu"><router-link to="/skills" class="link">About</router-link></li>
+            <li @click="mobileMenu"><router-link to="/jobs" class="link">Works</router-link></li>
+            <li @click="mobileMenu"><router-link to="/" class="link">Contact</router-link></li>
         </ul>
     </nav>
 </template>

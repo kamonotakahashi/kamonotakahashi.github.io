305d5f1b076b860428beaea439ff4cef0a115e61
kamonotakahashi <kamonotakahashi@gmail.com>
2020-04-17 00:51:59 +0900

    ホームを変更

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -7,13 +7,12 @@
                 :key="index"
                 :class="[line.className, { active : selectHeadlineContentNumber == index }]"
                 :data-column-name="line.className"
-                @click="clickActiveContent(index, line.vueExtendInstance, $event)"
+                @click="clickActiveContent(index, line.vueExtendInstance)"
             >
                 <div class="content__headline__title" :class="{ active : selectHeadlineContentNumber == index }">
                     {{line.text}}
                 </div>
-                <div class="">
-
+                <div class="content__headline__entry" v-show="selectHeadlineContentNumber == index">
                 </div>
             </div>
         </div>
@@ -41,23 +40,34 @@ import VueHomePortfolio from '@/components/parts/home/VueHomePortfolio.vue';
 })
 export default class About extends Mixins(StoreMixin) {
 
+    /* data */
     private headline: { className: string, text: string, vueExtendInstance: any  }[] = [
         { className: "portfolio", text: "Portfolio", vueExtendInstance: VueHomePortfolio},
         { className: "code-and-system", text: "Code And System", vueExtendInstance: VueHomePortfolio},
         { className: "works", text: "Works", vueExtendInstance: VueHomePortfolio}
     ];
 
-    private selectHeadlineContentNumber :number = 1;
+    private selectHeadlineContentNumber :number = 0;
 
-    private clickActiveContent(index: number, instance: any, $event: any) :void {
+    /* computed */
+    private clickActiveContent(index: number, instance: any) :void {
         this.selectHeadlineContentNumber = index;
-
         var ComponentClass = Vue.extend(instance);
         var componentInstance = new ComponentClass();
+        var contentInstance = document.getElementsByClassName("content__headline")[index];
+        contentInstance.getElementsByClassName("content__headline__entry")[0].innerHTML = '';
+        contentInstance.getElementsByClassName("content__headline__entry")[0].append(componentInstance.$mount().$el);
 
-        componentInstance.$mount();
+    };
 
-        console.log($event)
+    /* mounted */
+    mounted() {
+
+        //開いた時にデフォルトの選択コンテンツを動的バインド
+        this.clickActiveContent(
+            this.selectHeadlineContentNumber,
+            this.headline[this.selectHeadlineContentNumber].vueExtendInstance
+        );
 
     };
 

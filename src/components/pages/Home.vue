8647e7730cdd5b769eb04bae104b81e87a1b2f8e
kamonotakahashi <kamonotakahashi@gmail.com>
2020-04-25 02:58:51 +0900

    commit

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -3,7 +3,17 @@
         <div class="content">
             <div class="content__headline portfolio">
                 <div class="content__headline__title">
-                    <div class="content__headline__title-wrap">portfolio</div>
+                    <div class="content__headline__title-wrap">
+                    <VueLiner
+                        top="10"
+                        left="50"
+                        right="50"
+                        bottom="20"
+                        height="4"
+                        color="silver"
+                    />
+                        portfolio
+                    </div>
                 </div>
                 <div class="content__headline__entry">
                     <VueHomePortfolio></VueHomePortfolio>
@@ -25,10 +35,12 @@ import StoreMixin from '@/store/global';
 //Components
 import VueHomePortfolio from '@/components/parts/home/VueHomePortfolio.vue';
 import VueHomeSelfMadeContent from '@/components/parts/home/VueHomeSelfMadeContent.vue';
+import VueLiner from '@/components/parts/VueLiner.vue';
 
 @Component({
     components: {
-        VueHomePortfolio
+        VueHomePortfolio,
+        VueLiner
     }
 })
 export default class About extends Mixins(StoreMixin) {
--- src/components/parts/VueLiner.vue
+++ src/components/parts/VueLiner.vue
@@ -19,12 +19,18 @@ import { Component, Vue, Prop } from 'vue-property-decorator';
 export default class VueLiner extends Vue {
     @Prop({ default: 2, required: false }) top!: number;
     @Prop({ default: 2, required: false }) bottom!: number;
+    @Prop({ default: 0, required: false }) left!: number;
+    @Prop({ default: 0, required: false }) right!: number;
+    @Prop({ default: 0, required: false }) height!: number;
     @Prop({ default: 'silver', required: false }) color!: string;
 
     private get addStyle() :Object {
         return {
             "margin-top": `${this.top}px`,
-            "margin-bottom": `${this.bottom}px`
+            "margin-bottom": `${this.bottom}px`,
+            "margin-left": `${this.left}px`,
+            "margin-right": `${this.right}px`,
+            "border-width": `${this.height}px 0 0 0`
         };
     }
 
